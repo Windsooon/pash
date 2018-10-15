1848debe647b7d4f44102ec740dbaee1ba094a2a
Windson yang <wiwindson@outlook.com>
2018-10-15 22:33:05 +0800

    clean code

--- arg_parse.py
+++ arg_parse.py
@@ -3,23 +3,22 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-        'path', help='Spectify the path')
+        '-p',
+        dest='path',
+        help='Spectify the path')
 parser.add_argument(
         '-l', action='store_true',
         dest='long_format',
         help='use a long listing format')
 parser.add_argument(
-        '-a',
-        action='store_true',
+        '-a', action='store_true',
         dest='show_hidden',
         help='do not ignore entries starting with .')
 parser.add_argument(
-        '-S',
-        action='store_true',
+        '-S', action='store_true',
         dest='sort_by_size',
         help='sort by file size')
 parser.add_argument(
-        '-R',
-        action='store_true',
+        '-R', action='store_true',
         dest='list_subdir',
         help='list subdirectories recursively')
