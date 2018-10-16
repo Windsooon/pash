51d69b7b68e444a7ab82c788184b2c17e4571775
Windson yang <wiwindson@outlook.com>
2018-10-16 09:17:16 +0800

    put all tests in a dir

---
+++ tests/test_ls_function.py
@@ -0,0 +1,11 @@
+import os
+import unittest
+from unittest.mock import patch, MagicMock
+from List import LS
+
+
+class LsFunctionTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
