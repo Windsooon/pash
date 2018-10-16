a1f42ec417f16858976089b308b341aed03583c1
Windson yang <wiwindson@outlook.com>
2018-10-16 10:51:57 +0800

    add test for no path para

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,13 +1,19 @@
 import unittest
-from arg_parse import parser
+import os
+from arg_parse import parser, args
 
 
 class ParseTestCase(unittest.TestCase):
 
-    def test_parser(self):
-        args = parser.parse_args(['-p', 'hello', '-l', '-a', '-S', '-R'])
-        self.assertEqual(args.path, 'hello')
-        self.assertTrue(args.long_format)
-        self.assertTrue(args.show_hidden)
-        self.assertTrue(args.sort_by_size)
-        self.assertTrue(args.list_subdir)
+    def test_all_args_parser(self):
+        tem_args = parser.parse_args(['-p', 'hello', '-l', '-a', '-S', '-R'])
+        self.assertEqual(tem_args.path, 'hello')
+        self.assertTrue(tem_args.long_format)
+        self.assertTrue(tem_args.show_hidden)
+        self.assertTrue(tem_args.sort_by_size)
+        self.assertTrue(tem_args.list_subdir)
+
+    def test_no_path_args_parser(self):
+        tem_args = parser.parse_args(['-l'])
+        self.assertEqual(tem_args.path, None)
+        self.assertEqual(args.path, os.getcwd())
